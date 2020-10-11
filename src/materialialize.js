9412328eb6e60cf1a8ad9844580c34aeabf54d1f
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-10-11 15:08:11 +0300

    Add files via upload

---
+++ src/materialialize.js
@@ -0,0 +1,203 @@
+import "./styles.css";
+
+document.getElementById("materialialize").innerHTML = `
+<div class="row">
+<div class="col s8 offset-s2"><h1>Tic Tac Toe</h1></div>
+</div>
+
+
+  <div class="row">
+  <div class="col s1 offset-s3">
+    <div id="square11" class="NoClass">
+    
+      <button id="addtolist11">
+      <span id="text11"></span>
+      </button>
+    </div>
+    </div>
+    
+    <div class="col s1  ">
+    <div id="square12" class="NoClass">
+    
+      <button id="addtolist12">
+      <span id="text12" ></span>
+      </button>
+    </div>
+    </div>
+    <div class="col s1 ">
+    <div id="square13" class="NoClass">
+    
+      <button id="addtolist13"> 
+      <span id="text13" ></span>
+      </button>
+    </div>
+    </div>
+    <div class="col s1 ">
+    <div id="square14" class="NoClass">
+    
+      <button id="addtolist14">
+      <span id="text14"></span>
+      </button>
+    </div>
+    </div>
+    <div class="col s1 ">
+    <div id="square15" class="NoClass">
+   
+      <button id="addtolist15"> 
+      <span id="text15" ></span>
+      </button>
+    </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s1 offset-s3"> 
+    <div id="square21" class="NoClass">
+      <button id="addtolist21">
+        <span id="text21" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square22" class="NoClass">
+      <button id="addtolist22">
+        <span id="text22" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square23" class="NoClass">
+      <button id="addtolist23"> 
+        <span id="text23" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square24" class="NoClass">
+      <button id="addtolist24">
+        <span id="text24" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square25" class="NoClass">
+      <button id="addtolist25"> 
+        <span id="text25" ></span>
+      </button>
+    </div> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s1 offset-s3 "> 
+    <div id="square31" class="NoClass">
+      <button id="addtolist31">
+        <span id="text31" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square32" class="NoClass">
+      <button id="addtolist32">
+        <span id="text32" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square33" class="NoClass">
+      <button id="addtolist33"> 
+        <span id="text33" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square34" class="NoClass">
+      <button id="addtolist34">
+        <span id="text34" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square35" class="NoClass">
+      <button id="addtolist35"> 
+        <span id="text35" ></span>
+      </button>
+    </div> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s1 offset-s3 "> 
+    <div id="square41" class="NoClass">
+      <button id="addtolist41">
+        <span id="text41" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square42" class="NoClass">
+      <button id="addtolist42">
+        <span id="text42" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square43" class="NoClass">
+      <button id="addtolist43"> 
+        <span id="text43" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square44" class="NoClass">
+      <button id="addtolist44">
+        <span id="text44" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square45" class="NoClass">
+      <button id="addtolist45"> 
+        <span id="text45" ></span>
+      </button>
+    </div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col s1 offset-s3 "> 
+    <div id="square51" class="NoClass">
+      <button id="addtolist51">
+        <span id="text51" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square52" class="NoClass">
+      <button id="addtolist52">
+        <span id="text52" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square53" class="NoClass">
+      <button id="addtolist53"> 
+        <span id="text53" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square54" class="NoClass">
+      <button id="addtolist54">
+      <span id="text54" ></span>
+      </button>
+    </div> 
+    </div>
+    <div class="col s1 "> 
+    <div id="square55" class="NoClass">
+      <button id="addtolist55"> 
+        <span id="text55" ></span>
+      </button>
+    </div> 
+    </div>
+  </div>
+  
+
+
+`;
